0c4792ac14a125a9693db1b1d042327f078a7445
3omda <memad7628@gmail.com>
2024-09-16 23:14:16 +0300

    adding the local storage

--- index.js
+++ index.js
@@ -9,21 +9,32 @@ function addTask() {
         return;
     }
     else{
-        newTask.innerHTML = inputBox.value;
+        newTask.textContent = inputBox.value;
         listContainer.appendChild(newTask);
+        saveData();
     }
     let canselBtn = document.createElement("span");
-    canselBtn.innerHTML = "\u00d7";
+    canselBtn.textContent = "\u00d7";
     newTask.appendChild(canselBtn);
     inputBox.value = '';
+    saveData();
 }
 // checked and remove task with  click 
 listContainer.addEventListener("click", function(e){
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData()
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData()
     }
 }, false)
 
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showData() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showData()
